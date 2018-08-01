849b08efa3fe68d6a30a9f96b3a41c002308a512
Kyle Long <kylel95@vt.edu>
2018-08-01 02:15:34 -0400

    Update mergeArrays.py

--- mergeArrays.py
+++ mergeArrays.py
@@ -4,6 +4,6 @@ def mergeArray(arr):
     for i in range(size):
         for x in range(len(arr[i])):
             l.append(arr[i][x])
-    print(l)
-# arr = [[1, 3], [2, 4, 5], [0, 9, 10, 11]]
-# mergeArray(arr)
+    print(sorted(l))
+arr = [[1, 3], [2, 4, 5], [0, 9, 10, 11]]
+mergeArray(arr)
