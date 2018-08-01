a7bbf65e2c290cbc6d878fb8718d675502be2c63
Kyle Long <kylel95@vt.edu>
2018-08-01 11:56:51 -0400

    Add files via upload

---
+++ removeLarger.py
@@ -0,0 +1,8 @@
+def removeLarger(arr, n):
+    for i in arr:
+        if i > n:
+            arr.remove(i)
+    return arr
+list = [3, 4, 5, 6, 8, 9, 10, 452, 65, 2452, 75, 3424, 24, 563, 32, 445, 76, 87, 90, 34, 49, 12, 34]
+a = removeLarger(list, 0)
+print(a)
