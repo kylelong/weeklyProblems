829a36e00c24e3db2615c95c93fb617fccae576d
Kyle Long <kylelong@Kyles-MacBook-Pro-2.local>
2019-08-28 14:53:17 -0400

    Week of 8/25/2019 question

--- specialBag.rb
+++ specialBag.rb
@@ -1,8 +1,16 @@
+=begin
+Let's say you have some special bags that can only hold up to 3kgs of weight.
+Given an array of integers (where each integer is between 1 and 3) that
+represent weights, determine how many bags you need to carry all of them.
+
+@param arr the array representing the weight of the bags
+@return the number of bags needed to carry all weights
+=end
 def specialBag(arr)
     bags = (arr.sum.to_f / 3).ceil
     s = bags > 1 ? "bags" : "bag"
     "#{bags} " << s
 end
-puts specialBag([1,1,3,2])
-puts specialBag([2,2,2,1,3,1])
-puts specialBag([1,1,1])
+puts specialBag([1,1,3,2]) #3 bags
+puts specialBag([2,2,2,1,3,1]) #4 bags
+puts specialBag([1,1,1]) #1 bag
